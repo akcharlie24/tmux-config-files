875ccd5b367f4a5f5d7b8d9c2903ebc3bc3834ed
Akshat Bhardwaj <akshatbhardwaj24@gmail.com>
2024-05-19 15:50:26 +0530

    updated wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,9 +7,17 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "Catppuccin Macchiato"
+-- config.color_scheme = "Catppuccin Macchiato"
+config.color_scheme = "duskfox"
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.hide_tab_bar_if_only_one_tab = true
+
+config.window_padding = {
+	left = 5,
+	right = 5,
+	top = 6,
+	bottom = 0,
+}
 
 -- and finally, return the configuration to wezterm
 return config
